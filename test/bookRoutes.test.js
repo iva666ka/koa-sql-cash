31aa8d57c7175d6825db11c184f377a9f95031af
iva666ka <ivan.shesterkin@all.me>
2019-08-11 20:16:24 +0300

    enhance test's descriptions and asserts

--- test/bookRoutes.test.js
+++ test/bookRoutes.test.js
@@ -6,12 +6,12 @@ afterEach(() => {
 });
 
 describe('books routes', () => {
-  test('get /books without query', async () => {
+  test('get /books without query should return last 20 books', async () => {
     const result = await request(server).get('/books');
     expect(result.statusCode).toBe(200);
     expect(result.body.length).toBe(20);
   });
-  test('get /books with all params query', async () => {
+  test('get /books with all params query should return matched books', async () => {
     const result = await request(server).get('/books')
       .query({
         search: 'Some Author',
@@ -25,18 +25,19 @@ describe('books routes', () => {
     expect(result.body.length).toBe(2);
   });
 
-  test('get /books/:id with exist id', async () => {
+  test('get /books/:id with exist id should return book with this id', async () => {
     const result = await request(server).get('/books/99');
     expect(result.statusCode).toBe(200);
     expect(result.body.length).toBe(1);
+    expect(result.body[0].id).toBe(99);
   });
-  test('get /books/:id with wrong id', async () => {
+  test('get /books/:id with not existing id should return empty array', async () => {
     const result = await request(server).get('/books/99999999');
     expect(result.statusCode).toBe(200);
     expect(result.body.length).toBe(0);
   });
 
-  test('post /books create book', async () => {
+  test('post /books with req.body title and author should create book', async () => {
     const result = await request(server).post('/books')
       .send({
         title: 'test',
