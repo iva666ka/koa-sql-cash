a591a7140cc3f9bc4cde962f032aac91c5f53100
iva666ka <ivan.shesterkin@all.me>
2019-08-11 19:48:14 +0300

    add error handler

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const { allRouters } = require('./routes');
 
 const app = new Koa();
 
+// error handler
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (err.isJoi) {
+      err.status = 400;
+    }
+    ctx.status = err.status || 500;
+    ctx.body = { error: err.message };
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
+// display error log
+app.on('error', (err, ctx) => {
+  if (err.status === 500 || !err.status) console.log({ err, ctx });
+});
+
 // logger
 app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url}`);
